4de2904197ae2a3f5645a49cc979b863257b0bed
drage1994 <mdrage@live.no>
2020-09-23 13:26:32 +0200

    changes made to GPS

--- Project/app/src/main/java/cse/dit012/lost/Gps.java
+++ Project/app/src/main/java/cse/dit012/lost/Gps.java
@@ -1,80 +1,73 @@
 package cse.dit012.lost;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.location.Location;
-import android.util.Log;
+import android.os.Build;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.OnSuccessListener;
 
-import java.util.concurrent.Executor;
-
-public class Gps {
-    FusedLocationProviderClient fusedLocationProviderClient;
-    private final LatLng defaultLocation = new LatLng(-33.8523341, 151.2106085);
+public class Gps extends AppCompatActivity {
+    private static final int PERMISSIONS_FINE_LOCATION = 99;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LatLng location = new LatLng(-33.8523341, 151.2106085);
     private boolean locationPermissionGranted;
-    private Location location;
 
-    public Location getLocation(){
-        return location;
+
+    //Initialize in model by "LocationServices.getFusedLocationProviderClient(this) to access the phones gps
+    public Gps(FusedLocationProviderClient client){
+        fusedLocationProviderClient = client;
+    }
+    public LatLng getLocation(){
+        return new LatLng(location.latitude,location.longitude);
     }
 
-
-    public void updateLocation() {
-        /*
-         * Get the best and most recent location of the device, which may be null in rare
-         * cases when a location is not available.
-         */
-        try {
-            if (locationPermissionGranted) {
-                Task<Location> locationResult = fusedLocationProviderClient.getLastLocation();
-                locationResult.addOnCompleteListener((Executor) this, new OnCompleteListener<Location>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Location> task) {
-                        if (task.isSuccessful()) {
-                            // Set the map's camera position to the current location of the device.
-                            location = task.getResult();
-                            if (location != null) {
-                                LatLng current = new LatLng(location.getLatitude(),location.getLongitude());
-                            }
-                        } else {
-                            Log.d("Name", "Current location is null. ");
-                            Log.e("Name", "Exception: %s", task.getException());
-                        }
-                    }
-                });
+    public void updateGPS(Activity context){
+        //Checks if the user have granted the app access to the GPS
+        if(ActivityCompat.checkSelfPermission(context,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            //Access is granted, finding the location
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(context, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    //Got access, update location.
+                    updateLocation(location);
+                }
+            });
+        }
+        else{
+            //Checks if build version handles permission requests
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                ActivityCompat.requestPermissions((Activity) context,new String[] {Manifest.permission.ACCESS_FINE_LOCATION},PERMISSIONS_FINE_LOCATION);
             }
-        } catch (SecurityException e)  {
-            Log.e("Exception: %s", e.getMessage(), e);
         }
     }
 
-    public double retrieveLatitude(){
-        return (location.getLatitude());
-
+    private void updateLocation(Location newLocation) {
+        location = new LatLng(newLocation.getLatitude(),newLocation.getLongitude());
     }
 
-    public double retrieveLongitude(){
-        return (location.getLongitude());
-    }
-    /*
 
-    private void getPermession(){
+    public void onRequestPermissionsResult(Activity context,int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
-                android.Manifest.permission.ACCESS_FINE_LOCATION )
-                == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            locationPermissionGranted = true;
-        } else {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                    1);
+        switch (requestCode){
+            case PERMISSIONS_FINE_LOCATION:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    updateGPS(context);
+                }
+                else {
+                    Toast.makeText(context,"This app requires access to the location provider in order to work properly",Toast.LENGTH_SHORT);
+                    finish();
+                }
+                break;
         }
-
     }
-    */
-
 }
