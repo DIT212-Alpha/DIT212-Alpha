f9e8bfb5f71d887d4de7ff56e0482776497be575
Pixie Waffle <sophia_pham@hotmail.com>
2020-09-23 14:34:12 +0200

    get

--- Project/app/src/main/java/cse/dit012/lost/android/ui/map/BroadcastInfoWindowFragment.java
+++ Project/app/src/main/java/cse/dit012/lost/android/ui/map/BroadcastInfoWindowFragment.java
@@ -3,6 +3,7 @@ package cse.dit012.lost.android.ui.map;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,6 +13,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import cse.dit012.lost.Broadcast;
 import cse.dit012.lost.R;
@@ -20,6 +22,10 @@ import cse.dit012.lost.databinding.FragmentBroadcastInfoWindowBinding;
 public class BroadcastInfoWindowFragment extends Fragment {
     private static final String PARAM_COURSE = "course";
     private static final String PARAM_DESCRIPTION = "description";
+    private Button editInfoWindowButton;
+    private Button infoWindowCloseButton;
+    //private AlertDialog.Builder dialogBuilder;
+    //private AlertDialog dialog;
 
     private FragmentBroadcastInfoWindowBinding layoutBinding;
 
@@ -41,12 +47,15 @@ public class BroadcastInfoWindowFragment extends Fragment {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         layoutBinding = FragmentBroadcastInfoWindowBinding.inflate(inflater, container, false);
+
         return layoutBinding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        //dialogBuilder = new AlertDialog.Builder(this);
+        //final View popupInfoBox = getLayoutInflater().inflate(R.layout.fragment_broadcast_info_window, null);
 
         model = new ViewModelProvider(getActivity()).get(BroadcastInfoWindowViewModel.class);
 
@@ -54,5 +63,36 @@ public class BroadcastInfoWindowFragment extends Fragment {
         String description = getArguments().getString(PARAM_DESCRIPTION);
         layoutBinding.course.setText(course);
         layoutBinding.description.setText(description);
+
+        infoWindowCloseButton = (Button) layoutBinding.infoWindowCloseButton;
+        editInfoWindowButton = (Button) layoutBinding.editInfoWindowButton;
+
+        //dialogBuilder.setView(popupInfoBox);
+        //dialog = dialogBuilder.create();
+        //dialog.show();
+
+        infoWindowCloseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //close View
+                //dialog.dismiss();
+                getActivity().getFragmentManager().popBackStack(); //ska stänga fönstret men funkar ej
+            }
+        });
+
+        editInfoWindowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //editButton - Delete course and description and add EditText?
+            }
+        });
+
+
+
     }
+
+
+
+
 }
