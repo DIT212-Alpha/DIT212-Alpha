ea15075034c52339eccd860b87fd524a92954caf
Bashar <basharaumary@gmail.com>
2020-09-29 15:59:00 +0200

    added loginFragment and registerFragment for authentication

--- Project/app/src/main/java/cse/dit012/lost/fragments/LoginFragment.java
+++ Project/app/src/main/java/cse/dit012/lost/fragments/LoginFragment.java
@@ -1,5 +1,6 @@
 package cse.dit012.lost.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,8 +11,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
@@ -25,12 +31,22 @@ import cse.dit012.lost.R;
 public class LoginFragment extends Fragment  {
 
     Button loginButton;
+    TextView textViewNewUser;
+
+    EditText editTextEmail;
+    EditText editTextPassword;
+
+    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         return  inflater.inflate(R.layout.fragment_login,container,false);
+
+
 
     }
 
@@ -39,12 +55,22 @@ public class LoginFragment extends Fragment  {
         super.onViewCreated(view, savedInstanceState);
 
         /**
-         * Initialize the navigation controller and vhange fragment on click
+         * Initialize the navigation controller and change fragment on click
          */
         final NavController navController = Navigation.findNavController(view);
-        loginButton = view.findViewById(R.id.buttonFragment);
-        loginButton.setOnClickListener(view1 -> navController.navigate(R.id.action_loginFragment_to_mapScreenFragment));
+
+        textViewNewUser = view.findViewById(R.id.clickable_text_new_user);
+
+        loginButton = view.findViewById(R.id.cirLoginButton);
+        textViewNewUser.setOnClickListener(view1 -> navController.navigate(R.id.action_loginFragment_to_registerFragment));
+        editTextEmail = view.findViewById(R.id.editTextEmail);
+        editTextPassword = view.findViewById(R.id.editTextPassword);
+
 
 
     }
+
+
+
+
 }
---
+++ Project/app/src/main/java/cse/dit012/lost/fragments/RegisterFragment.java
@@ -0,0 +1,132 @@
+package cse.dit012.lost.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.core.utilities.Utilities;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import cse.dit012.lost.R;
+import cse.dit012.lost.User;
+
+
+public class RegisterFragment extends Fragment {
+
+EditText userName;
+EditText surName;
+EditText userEmail;
+EditText userPassword;
+
+Button registerButton;
+
+ProgressBar progressBar;
+
+User user;
+
+private FirebaseAuth registerAuthentication;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false);
+
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        progressBar =  view.findViewById(R.id.progressBar);
+        registerAuthentication = FirebaseAuth.getInstance();
+
+        userName = view.findViewById(R.id.registerTextUserName);
+        surName = view.findViewById(R.id.registerTextSurName);
+        userEmail = view.findViewById(R.id.registerTextEmail);
+        userPassword = view.findViewById(R.id.registerTextPassword);
+        registerButton = view.findViewById(R.id.cirRegisterButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = userEmail.getText().toString();
+                String password = userPassword.getText().toString();
+
+                if (validate(email,password)){
+
+                    registerAuthentication.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful()){
+                                Toast.makeText(getContext(),"Registration Sucsessful!", Toast.LENGTH_LONG).show();
+                                progressBar.setVisibility(View.GONE);
+                            }
+                        }
+                    });
+                }
+
+            }
+        });
+
+
+    }
+
+    private boolean validate(String email, String password) {
+        progressBar.setVisibility(View.VISIBLE);
+        // Reset errors.
+        userName.setError(null);
+        userPassword.setError(null);
+
+        if (TextUtils.isEmpty(email)) {
+            userEmail.setError("Email is required");
+            return false;
+        } else if (!isEmailValid(email)) {
+            userEmail.setError("Enter a valid email");
+            return false;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            userPassword.setError("Password is required");
+            return false;
+        } else if (!isPasswordValid(password)) {
+            userPassword.setError("Password must contain at least 6 characters");
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean isEmailValid(String email){
+        Pattern pattern = Patterns.EMAIL_ADDRESS;
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    //Check password with minimum requirement here(it should be minimum 6 characters)
+    public static boolean isPasswordValid(String password){
+        return password.length() >= 6;
+    }
+
+  
+}
