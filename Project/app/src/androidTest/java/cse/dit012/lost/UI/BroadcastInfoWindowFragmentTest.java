af10e97297cab006f7a1499f55b3b4857f79ecc6
Pixie Waffle <sophia_pham@hotmail.com>
2020-10-04 12:46:48 +0200

    BroadcastInfoWindowTest

---
+++ Project/app/src/androidTest/java/cse/dit012/lost/UI/BroadcastInfoWindowFragmentTest.java
@@ -0,0 +1,36 @@
+package cse.dit012.lost.UI;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+
+class BroadcastInfoWindowFragmentTest {
+
+    //The course displayed should be the same as the one in the database
+    @Test
+    public void onViewCreatedCourse() {
+        
+    }
+
+    //The description displayed should be the same as the one in the database
+    @Test
+    public void onViewCreatedDescription() {
+
+    }
+
+    //The course edited should be updated in the database
+    @Test
+    public void editCourseInfoWindow() {
+
+    }
+
+    //The description edited should be updated in the database
+    @Test
+    public void editDescriptionInfoWindow() {
+
+    }
+
+
+}
