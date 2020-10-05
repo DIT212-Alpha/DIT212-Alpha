0521bbed3c5849be861e57c5670affd97af85a1c
drage1994 <mdrage@live.no>
2020-10-05 15:29:35 +0200

    added instrumental tests for registrationscreenfragment

--- Project/app/src/androidTest/java/cse/dit012/lost/RegistrationTest.java
+++ Project/app/src/androidTest/java/cse/dit012/lost/RegistrationTest.java
@@ -3,17 +3,19 @@ package cse.dit012.lost;
 import android.app.Activity;
 import android.content.Context;
 
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.testing.FragmentScenario;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.lang.reflect.AccessibleObject;
-
 import cse.dit012.lost.android.ui.MainActivity;
+import cse.dit012.lost.android.ui.screen.welcome.RegistrationScreenFragment;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
@@ -22,9 +24,11 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
-public class LoginTest {
-    @Rule
-    public ActivityTestRule<MainActivity> rule = new ActivityTestRule(MainActivity.class);
+public class RegistrationTest {
+    @Before
+    public void setUp(){
+        FragmentScenario.launchInContainer(RegistrationScreenFragment.class);
+    }
 
     @Test
     public void useAppContext() {
@@ -33,16 +37,20 @@ public class LoginTest {
         assertEquals("cse.dit012.lost", appContext.getPackageName());
     }
     @Test
-    public void typeUsername(){
-        onView(withId(R.id.editTextEmail)).perform(typeText("Mathias"));
+    public void typeEmail(){
+        onView(withId(R.id.registerTextEmail)).perform(typeText("gusdragema@student.gu.se"));
     }
     @Test
-    public void typePassword(){
-        onView(withId(R.id.editTextPassword)).perform((typeText("hejhejhej")));
+    public void typeName(){
+        onView(withId(R.id.registerTextUserName)).perform(typeText("Mathias"));
     }
     @Test
-    public void clickLogin(){
-        onView(withId(R.id.cirLoginButton)).perform(click());
+    public void typeSurName(){
+        onView(withId(R.id.registerTextSurName)).perform(typeText("Drage"));
+    }
+    @Test
+    public void typePw(){
+        onView(withId(R.id.registerTextPassword)).perform(typeText("hejhejhej"));
     }
     @Test
     public void clickRegister(){
